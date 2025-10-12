d91cfb039d38912830654a588602a6aea9f7fb47
rohithpeyarusahib <itsmerohu31@gmail.com>
2025-10-12 11:56:05 +0530

    fixed some errors.

--- .next/server/middleware-build-manifest.js
+++ .next/server/middleware-build-manifest.js
@@ -1,23 +1,27 @@
 globalThis.__BUILD_MANIFEST = {
   "pages": {
-    "/_app": []
+    "/_app": [
+      "static/chunks/e60ef129113f6e24.js",
+      "static/chunks/871ab61f5e45de2e.js",
+      "static/chunks/turbopack-380e24004c867c1c.js"
+    ],
+    "/_error": [
+      "static/chunks/17722e3ac4e00587.js",
+      "static/chunks/871ab61f5e45de2e.js",
+      "static/chunks/turbopack-730952d6b9212bcd.js"
+    ]
   },
   "devFiles": [],
   "ampDevFiles": [],
   "polyfillFiles": [
-    "static/chunks/node_modules_next_dist_build_polyfills_polyfill-nomodule.js"
+    "static/chunks/a6dad97d9634a72d.js"
   ],
   "lowPriorityFiles": [],
   "rootMainFiles": [
-    "static/chunks/[turbopack]_browser_dev_hmr-client_hmr-client_ts_57d40746._.js",
-    "static/chunks/node_modules_next_dist_compiled_react-dom_1e674e59._.js",
-    "static/chunks/node_modules_next_dist_compiled_next-devtools_index_a9cb0712.js",
-    "static/chunks/node_modules_next_dist_compiled_5150ccfd._.js",
-    "static/chunks/node_modules_next_dist_client_cf1d9188._.js",
-    "static/chunks/node_modules_next_dist_b0daae9a._.js",
-    "static/chunks/node_modules_@swc_helpers_cjs_b3dc30d6._.js",
-    "static/chunks/_a0ff3932._.js",
-    "static/chunks/turbopack-_cdba956c._.js"
+    "static/chunks/af565aa9e0364985.js",
+    "static/chunks/f471ac2339220d44.js",
+    "static/chunks/ff42156ceacb9fd8.js",
+    "static/chunks/turbopack-077adbdbd71e226c.js"
   ],
   "ampFirstPages": []
 };
--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,6 @@
-import Hero from "./components/Hero";
+"use client";
+
+import dynamic from "next/dynamic";
 import About from "./components/About";
 import Experience from "./components/Experience";
 import Projects from "./components/Projects";
@@ -6,6 +8,8 @@ import Skills from "./components/Skills";
 import Certifications from "./components/Certifications";
 import Education from "./components/Education";
 import Contact from "./components/Contact";
+
+const Hero = dynamic(() => import("./components/Hero"), { ssr: false });
 
 export default function Home() {
   return (
